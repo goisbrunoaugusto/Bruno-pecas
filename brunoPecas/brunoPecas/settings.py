2ada68a081755f5cee4319b9ad0e81a7bc2a73e6
Bruno Augusto <brunogois902@gmail.com>
2025-02-14 18:56:57 -0300

    docs: added swagger

--- brunoPecas/brunoPecas/settings.py
+++ brunoPecas/brunoPecas/settings.py
@@ -39,8 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #third party apps
     'rest_framework',
     'rest_framework_simplejwt',
+    'drf_yasg',
+    #local apps
     'users',
     'parts',
     'cars'
